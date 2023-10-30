9346d4bee264935f88ca327c4fd1a27c9b3112a5
Papito <fernandorcks@gmail.com>
2023-10-30 11:28:52 -0300

    Delete hope-po directory

--- hope-po/web/cypress/e2e/home.cy.js
+++
@@ -1,8 +0,0 @@
-describe('home spec', () => {
-  it('hope web deve estar online', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.get('h1')
-      .should('have.text', 'Semeando esperança, colhendo sorrisos')
-  })
-})
